6779e2068026bef0e92ec2b9f432545c6b045e27
David Haoming Chen <hchen78@sheffield.ac.uk>
2022-08-12 15:56:41 +0100

    python tts implementation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PIL import Image
 from pytesseract import pytesseract
 import os
-
+import pyttsx3
+s = pyttsx3.init()
 #Define path to tessaract.exe
 path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -21,46 +22,7 @@ for root, dirs, file_names in os.walk(path_to_images):
         #Extract text from image
         text = pytesseract.image_to_string(img)
 
-        print(text)
+        s.say(text)
+        s.runAndWait()
 
 
-# import subprocess
-#
-# # getting meta data of the wifi network
-# meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
-#
-# # decoding meta data from byte to string
-# data = meta_data.decode('utf-8', errors="backslashreplace")
-#
-# # splitting data by line by line
-# # string to list
-# data = data.split('\n')
-#
-# # creating a list of wifi names
-# names = []
-#
-# # traverse the list
-# for i in data:
-#
-#     # find "All User Profile" in each item
-#     # as this item will have the wifi name
-#     if "All User Profile" in i:
-#         # if found split the item
-#         # in order to get only the name
-#         i = i.split(":")
-#
-#         # item at index 1 will be the wifi name
-#         i = i[1]
-#
-#         # formatting the name
-#         # first and last chracter is use less
-#         i = i[1:-1]
-#
-#         # appending the wifi name in the list
-#         names.append(i)
-#
-# # printing the wifi names
-# print("All wifi that system has connected to are ")
-# print("-----------------------------------------")
-# for name in names:
-#     print(name)
